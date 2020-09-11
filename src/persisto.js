83caf1983e9d01a0f52702868b91e36077f03d79
Ivan Vergés <ivan@platoniq.net>
2020-09-11 17:20:35 +0200

    fix var assignation

--- src/persisto.js
+++ src/persisto.js
@@ -520,12 +520,12 @@
     /** Write data to form elements with the same name.
      */
     writeToForm: function(form, options) {
-      var $form = $(form);
-      self = this;
+      var $form = $(form),
+        self = this;
 
       $.each(this._data, function(k) {
-        var v = self.get(k);
-        var $input = $form.find("[name='" + k + "']"),
+        var v = self.get(k),
+          $input = $form.find("[name='" + k + "']"),
           type = $input.attr("type");
 
         if ($input.length) {
